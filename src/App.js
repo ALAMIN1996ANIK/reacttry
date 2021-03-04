56daf146731c704bb806a1a5958c00b4da506085
ALAMIN1996ANIK <alamin1996ani@gmail.com>
2021-03-04 14:42:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Players from './components/Players/Players';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <main className='py-3'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-9'>
+              <Players></Players>
+            </div>
+            <div className='col-lg-3'>Player Price</div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
